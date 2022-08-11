b5f6ef840d703e4ff13fe60deeea5dbe9d896032
wayne.k <wayne.k@kakaostyle.com>
2022-08-11 12:48:59 +0900

    체인지로그 스케줄러 서비스 별도로 분리

---
+++ changelogservice.js
@@ -0,0 +1,132 @@
+import { AppStoreService } from './appstoreservice.js';
+import { v4 as uuidv4 } from 'uuid';
+import { ToadScheduler, SimpleIntervalJob, AsyncTask } from 'toad-scheduler';
+
+export class ChangelogService {
+  constructor(issuerId, keyId, key) {
+    this.appStoreService = new AppStoreService(issuerId, keyId, key);
+    this.scheduler = new ToadScheduler();
+    this.tryCountCache = {}; // { uuid: count }
+  }
+
+  startScheduledJob(appId, buildNumber, changelog) {
+    // Validate parameters
+    if (!this.#validateChangelogParameters(appId, buildNumber, changelog)) {
+      return false
+    }
+    console.log(`[DEBUG] AppId: ${ appId }, BuildNumber: ${ buildNumber }, Changelog: ${ changelog }`);
+
+    // Set Cache
+    const uuid = uuidv4();
+    console.log('[DEBUG] UUID: ' + uuid);
+
+    this.tryCountCache[uuid] = 0;
+
+    // Make AsyncTask
+    const maxTryCount = process.env.RETRY_COUNT || 10;
+    const asyncTask = new AsyncTask(uuid, async () => {
+      const tryCount = this.tryCountCache[uuid];
+  
+      console.log(`===== Task Started (${tryCount + 1} of ${maxTryCount}) =====`);
+  
+      try {
+        // Builds
+        const builds = await this.appStoreService.getBuilds(appId);
+        const build = builds.find(build => build.version === buildNumber);
+  
+        if (!build) {
+          if (tryCount >= maxTryCount) {
+            throw Error('[Error] Timeout.');
+          } else {
+            this.tryCountCache[uuid] = tryCount + 1;
+            console.log(`[VERBOSE] Can't found build, the task will resume.`);
+            return
+          }
+        } else if (build.expired) {
+          throw Error('[ERROR] Expired build: ' + buildNumber);
+        }
+  
+        console.log('[DEBUG] Build founded.');
+        console.log(build);
+  
+        // Localization
+        const localization = await this.appStoreService.getLocalization(build.id);
+  
+        if (!localization) {
+          throw Error('[ERROR] Localization not found.');
+        } else if (localization.whatsNew) {
+          throw Error('[ERROR] Changelog already exist.');
+        }
+  
+        console.log('[DEBUG] Localization received.');
+        console.log(localization);
+  
+        // SetChangelog
+        const resultOfSetChangelog = await this.appStoreService.setChangelog(localization.id, changelog);
+        console.log('[DEBUG] SetChangelog finished.');
+        console.log(resultOfSetChangelog);
+  
+        // SetUsesNonExemptEncryption
+        const resultOfSetEncryption = await this.appStoreService.setUsesNonExemptEncryption(build.id);
+        console.log('[DEBUG] setEncryption finished.');
+        console.log(resultOfSetEncryption);
+  
+        // Finish
+        this.#removeJobById(uuid);
+        console.log(`===== Task Finished (${tryCount + 1} of ${maxTryCount}) =====`);
+      } catch (error) {
+        throw error;
+      }
+    }, (error) => {
+      console.log(error);
+      this.#removeJobById(uuid);
+    })
+  
+    // Make IntervalJob
+    const interval = process.env.INTERVAL_MINUTE || 3;
+    const job = new SimpleIntervalJob({ minutes: interval }, asyncTask, uuid);
+  
+    // Set Scheduler
+    this.scheduler.addSimpleIntervalJob(job);
+
+    return true
+  }
+
+  #removeJobById(uuid) {
+    this.scheduler.removeById(uuid);
+    delete this.tryCountCache[uuid];
+  }
+
+  #validateChangelogParameters(appId, buildNumber, changelog) {
+    function isValid(value) {
+      if (!value) {
+        console.log('value is null');
+        return false
+      }
+    
+      if (value.length <= 0) {
+        console.log('value is empty');
+        return false
+      }
+    
+      return true
+    }
+
+    if (!isValid(appId)) {
+      console.log('invalid app_id');
+      return false
+    }
+
+    if (!isValid(buildNumber)) {
+      console.log('invalid build_number');
+      return false
+    }
+
+    if (!isValid(changelog)) {
+      console.log('invalid changelog');
+      return false
+    }
+
+    return true
+  }
+}
--- index.js
+++ index.js
@@ -2,20 +2,16 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import dotEnv from 'dotenv';
 import fs from 'fs';
-import { AppStoreService } from './appstoreservice.js';
-import { v4 as uuidv4, validate } from 'uuid';
-import { ToadScheduler, SimpleIntervalJob, AsyncTask } from 'toad-scheduler';
+import { ChangelogService } from './changelogservice.js';
 
 dotEnv.config({ path: './env/.env' });
 
 const PORT = process.env.PORT || 4000;
-const appStoreService = new AppStoreService(
+const changelogService = new ChangelogService(
   process.env.ISSUER_ID,
   process.env.KEY_ID,
   fs.readFileSync("./env/certificate.p8")
 );
-const scheduler = new ToadScheduler();
-const tryCountCache = {}; // { uuid: count }
 
 express()
   .get('/', function (_, res) {
@@ -28,128 +24,18 @@ express()
   .post('/changelog', function (req, res) {
     console.log('[POST] changelog');
 
-    if (!validateChangelogParameters(req)) {
+    // Start Scheduled Job
+    if (!changelogService.startScheduledJob(
+      req.body.app_id, 
+      req.body.build_number, 
+      req.body.changelog)
+    ) {
       res.status(400).send('Invalid parameters.');
       return
     }
 
-    // Set Cache
-    const uuid = uuidv4();
-    console.log('[DEBUG] UUID: ' + uuid);
-
-    tryCountCache[uuid] = 0;
-
-    // Set Scheduler
-    startScheduledJob(
-      uuid, 
-      req.body.app_id, 
-      req.body.build_number, 
-      req.body.changelog
-    );
-
     res.sendStatus(200);
   })
-  .listen(PORT, () => console.log(`Listening on ${PORT}`));
-
-function validateChangelogParameters(req) {
-  if (!isValidParameter(req.body.app_id)) {
-    console.log('invalid app_id');
-    return false
-  }
-
-  if (!isValidParameter(req.body.build_number)) {
-    console.log('invalid build_number');
-    return false
-  }
-  
-  if (!isValidParameter(req.body.changelog)) {
-    console.log('invalid changelog');
-    return false
-  }
-
-  return true
-}
-
-function isValidParameter(value) {
-  if (!value) {
-    console.log('value is null');
-    return false
-  }
-
-  if (value.length <= 0) {
-    console.log('value is empty');
-    return false
-  }
-
-  return true
-}
-
-function startScheduledJob(uuid, appId, buildNumber, changeLog) {
-  const maxTryCount = process.env.RETRY_COUNT || 10;
-  
-  // Make AsyncTask
-  const asyncTask = new AsyncTask(uuid, async () => {
-    const tryCount = tryCountCache[uuid];
-
-    console.log(`===== Task Started (${ tryCount + 1 } of ${ maxTryCount }) =====`);
-
-    try {
-      const builds = await appStoreService.getBuilds(appId);
-      const build = builds.find(build => build.version === buildNumber);
-
-      if (!build) {
-        if (tryCount >= maxTryCount) {
-          throw Error('[Error] Timeout.');
-        } else {
-          tryCountCache[uuid] = tryCount + 1;
-          console.log(`[VERBOSE] Can't found build, the task will resume.`);
-          return
-        }
-      } else if (build.expired) {
-        throw Error('[ERROR] Expired build: ' + buildNumber);
-      }
-      
-      console.log('[DEBUG] Build founded.');
-      console.log(build);
-      
-      const localization = await appStoreService.getLocalization(build.id);
-
-      if (!localization) {  
-        throw Error('[ERROR] Localization not found.');
-      } else if (localization.whatsNew) {
-        throw Error('[ERROR] Changelog already exist.');
-      }
-      
-      console.log('[DEBUG] Localization received.');
-      console.log(localization);
-
-      const resultOfSetChangelog = await appStoreService.setChangelog(localization.id, changeLog);
-      console.log('[DEBUG] SetChangelog finished.');
-      console.log(resultOfSetChangelog);
-
-      const resultOfSetEncryption = await appStoreService.setUsesNonExemptEncryption(build.id);
-      console.log('[DEBUG] setEncryption finished.');
-      console.log(resultOfSetEncryption);
-
-      removeJobById(uuid);
-      console.log(`===== Task Finished (${ tryCount + 1 } of ${ maxTryCount }) =====`);
-    } catch (error) {
-      throw error;
-    }
-  }, (error) => {
-    console.log(error);
-    removeJobById(uuid);
-  })
-
-  // Make IntervalJob
-  const interval = process.env.INTERVAL_MINUTE || 3;
-  const job = new SimpleIntervalJob({ minutes: interval }, asyncTask, uuid);
-
-  // Set Scheduler
-  scheduler.addSimpleIntervalJob(job);
-}
-
-function removeJobById(uuid) {
-  scheduler.removeById(uuid);
-  delete tryCountCache[uuid];
-}
+  .listen(PORT, () => {
+    console.log(`Listening on ${PORT}`)
+  });
