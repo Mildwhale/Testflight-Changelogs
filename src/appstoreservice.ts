2ff8181fb8e350e1b8eb425e51e05990852cc451
wayne.k <wayne.k@kakaostyle.com>
2022-08-14 01:22:55 +0900

    앱스토어 서비스 추가

---
+++ src/appstoreservice.ts
@@ -0,0 +1,156 @@
+import * as jwt from 'jsonwebtoken';
+import axios, { AxiosRequestConfig } from 'axios';
+
+export interface Build {
+  id: string;
+  version: string;
+  expired: boolean;
+  processingState: string;
+  usesNonExemptEncryption?: boolean;
+}
+
+export interface Localization {
+  id: string;
+  whatsNew: string;
+  locale: string;
+}
+
+export class AppStoreService {
+  private baseUrl = 'https://api.appstoreconnect.apple.com/v1';
+  private issuerId: string
+  private keyId: string
+  private key: string
+
+  constructor(issuerId: string, keyId: string, key: string) {
+    this.issuerId = issuerId;
+    this.keyId = keyId;
+    this.key = key;
+  }
+
+  public async getBuilds(appId: string, limit = 5): Promise<[Build]> {
+    const api = '/builds';
+    const query = '?filter[app]=' + appId + '&limit=' + limit + '&sort=-uploadedDate';
+    const url = this.baseUrl + api + query;
+
+    try {
+      const response = await axios.get(url, this.getConfig());
+
+      return response.data.data.map((build: any): Build =>
+      ({
+        id: build.id,
+        version: build.attributes.version,
+        expired: build.attributes.expired,
+        processingState: build.attributes.processingState,
+        usesNonExemptEncryption: build.attributes.usesNonExemptEncryption
+      })
+      );
+    } catch (error) {
+      // Handle error
+      throw error;
+    }
+  }
+
+  public async getLocalization(buildId: string): Promise<Localization> {
+    const api = '/builds';
+    const url = this.baseUrl + api + `/${buildId}` + '/betaBuildLocalizations';
+
+    try {
+      const response = await axios.get(url, this.getConfig());
+      const localization: any = response.data.data[0];
+
+      return {
+        id: localization.id,
+        whatsNew: localization.attributes.whatsNew,
+        locale: localization.attributes.locael
+      }
+    } catch (error) {
+      // Handle error
+      throw error;
+    }
+  }
+
+  public async setChangelog(localizationId: string, changelog: string): Promise<Localization> {
+    const api = '/betaBuildLocalizations';
+    const url = this.baseUrl + api + `/${localizationId}`;
+    const data = {
+      data: {
+        id: `${localizationId}`,
+        type: 'betaBuildLocalizations',
+        attributes: {
+          whatsNew: `${changelog}`
+        }
+      }
+    };
+
+    try {
+      const response = await axios.patch(url, data, this.getConfig());
+      const localization = response.data.data;
+
+      return {
+        id: localization.id,
+        whatsNew: localization.attributes.whatsNew,
+        locale: localization.attributes.locael
+      }
+    } catch (error) {
+      // Handle error
+      throw error;
+    }
+  }
+
+  public async setUsesNonExemptEncryption(buildId: string): Promise<Build> {
+    const api = '/builds';
+    const url = this.baseUrl + api + `/${buildId}`;
+    const data = {
+      data: {
+        id: `${buildId}`,
+        type: 'builds',
+        attributes: {
+          usesNonExemptEncryption: false
+        }
+      }
+    };
+
+    try {
+      const response = await axios.patch(url, data, this.getConfig());
+      const build = response.data.data;
+      
+      return {
+        id: build.id,
+        version: build.attributes.version,
+        expired: build.attributes.expired,
+        processingState: build.attributes.processingState,
+        usesNonExemptEncryption: build.attributes.usesNonExemptEncryption
+      }
+    } catch (error) {
+      // Handle error
+      throw error;
+    }
+  }
+
+  private generateJwtToken(): string {
+    const payload = {
+      iss: this.issuerId,
+      aud: 'appstoreconnect-v1',
+      exp: Math.floor(Date.now() / 1000) + (2 * 60)
+    };
+
+    return jwt.sign(
+      payload,
+      this.key,
+      {
+        algorithm: 'ES256',
+        keyid: this.keyId
+      }
+    );
+  }
+
+  private getConfig(): AxiosRequestConfig {
+    const jwtToken = this.generateJwtToken();
+    return {
+      headers: {
+        'Authorization': `Bearer ${jwtToken}`,
+        'Content-Type': 'application/json'    
+      }
+    }
+  }
+}
